4547e23600c66bc2dc902299c91160383ba7a104
SvenDowideit <SvenDowideit>
2008-10-29 00:44:48 +0000

    initial removeal of outdated plugins - does the mailing list cope

--- twikiplugins/WikiwygContrib/lib/TWiki/Contrib/WikiwygContrib.pm
+++
@@ -1,4 +0,0 @@
-package TWiki::Contrib::WikiwygContrib;
-use vars qw( $VERSION );
-$VERSION = '$Rev$';
-1;
--- twikiplugins/WikiwygContrib/test/unit/WikiwygContrib/WikiwygContribSuite.pm
+++
@@ -1,9 +0,0 @@
-package WikiwygContribSuite;
-
-use base qw(Unit::TestSuite);
-
-sub name { 'WikiwygContribSuite' };
-
-sub include_tests { qw(WikiwygContribTests) };
-
-1;
