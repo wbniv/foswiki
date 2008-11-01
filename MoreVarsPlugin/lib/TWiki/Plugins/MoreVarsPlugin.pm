45090f584802eb09e328c14f738eae0893853b68
SvenDowideit <SvenDowideit>
2008-11-01 17:17:47 +0000

    import MoreVarsPlugin

---
+++ MoreVarsPlugin/lib/TWiki/Plugins/MoreVarsPlugin.pm
@@ -0,0 +1,102 @@
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
+# Copyright (C) 2001-2006 Peter Thoeny, peter@thoeny.org
+# and TWiki Contributors. All Rights Reserved. TWiki Contributors
+# are listed in the AUTHORS file in the root of this distribution.
+# NOTE: Please extend that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For licensing info read LICENSE file in the TWiki root.
+
+=pod
+
+---+ package MoreVarsPlugin
+
+Some cheap variable-like replacements to make it easier for ordinary
+people to use them
+
+=cut
+
+# change the package name and $pluginName!!!
+package TWiki::Plugins::MoreVarsPlugin;
+
+# Always use strict to enforce variable scoping
+use strict;
+
+# $VERSION is referred to by TWiki, and is the only global variable that
+# *must* exist in this package
+use vars qw( $VERSION $RELEASE $debug $pluginName );
+
+# This should always be $Rev$ so that TWiki can determine the checked-in
+# status of the plugin. It is used by the build automation tools, so
+# you should leave it alone.
+$VERSION = '$Rev$';
+
+# This is a free-form string you can use to "name" your own plugin version.
+# It is *not* used by the build automation tools, but is reported as part
+# of the version number in PLUGINDESCRIPTIONS.
+$RELEASE = 'Dakar';
+
+# Name of this Plugin, only used in this module
+$pluginName = 'MoreVarsPlugin';
+
+=pod
+
+---++ initPlugin($topic, $web, $user, $installWeb) -> $boolean
+   * =$topic= - the name of the topic in the current CGI query
+   * =$web= - the name of the web in the current CGI query
+   * =$user= - the login name of the user
+   * =$installWeb= - the name of the web the plugin is installed in
+
+REQUIRED
+
+Called to initialise the plugin. If everything is OK, should return
+a non-zero value. On non-fatal failure, should write a message
+using TWiki::Func::writeWarning and return 0. In this case
+%FAILEDPLUGINS% will indicate which plugins failed.
+
+In the case of a catastrophic failure that will prevent the whole
+installation from working safely, this handler may use 'die', which
+will be trapped and reported in the browser.
+
+You may also call =TWiki::Func::registerTagHandler= here to register
+a function to handle tags that have standard TWiki syntax - for example,
+=%MYTAG{"my param" myarg="My Arg"}%. You can also override internal
+TWiki tag handling functions this way, though this practice is unsupported
+and highly dangerous!
+
+=cut
+
+sub initPlugin {
+    my( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $TWiki::Plugins::VERSION < 1.026 ) {
+        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+        return 0;
+    }
+
+    TWiki::Func::registerTagHandler( 'ORIGINALAUTHOR', \&_ORIGINALAUTHOR );
+
+    # Plugin correctly initialized
+    return 1;
+}
+
+sub _ORIGINALAUTHOR {
+    my($session, $params, $theTopic, $theWeb) = @_;
+    my $aWeb = $params->{web} || $theWeb;
+    my $aTopic = $params->{topic} || $theTopic;
+
+    my ($date, $user, $rev, $comment) = TWiki::Func::getRevisionInfo($aWeb, $aTopic, "1");
+    return TWiki::Func::getMainWebname() . '.' . $user;
+}
