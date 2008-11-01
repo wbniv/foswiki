e1968d2cb20ad0b7a7b0d8b5e03be976cc88765e
SvenDowideit <SvenDowideit>
2008-11-01 14:35:35 +0000

    begin getting autobuilds started

--- tools/buildTWikiRelease.pl
+++ tools/buildTWikiRelease.pl
@@ -69,8 +69,8 @@ unless ($errorcode == 0) {
     
     chdir($twikihome);
     if ($SvensAutomatedBuilds) {
-    	`scp TWiki* fosiki\@fosiki.com:/home/distributedinformation/www/TWikiBuilds`;
     	sendEmail('nextwiki-svn@lists.sourceforge.net', "Subject: NextWiki $twikiBranch branch has Unit test FAILURES\n\n see http://fosiki.com/$twikiBranch/ for output files.\n".$unittestErrors);
+    	`scp TWiki* fosiki\@fosiki.com:/home/distributedinformation/www/TWikiBuilds`;
     }
     die "\n\n$errorcode: unit test failures - need to fix them first\n" 
 }
@@ -108,11 +108,11 @@ chdir('lib');
 chdir($twikihome);
 if ($SvensAutomatedBuilds) {
 	#push the files to my server - http://fosiki.com/TWikiBuilds/
-	`scp TWiki* fosiki\@fosiki.com:/home/distributedinformation/www/$twikiBranch` ;
 	my $buildOutput = `ls -alh *auto*`;
 	$buildOutput .= "\n";
 	$buildOutput .= `grep 'All tests passed' $twikihome/TWiki-UnitTests.log`;
 	sendEmail('Builds@fosiki.com', "Subject: NextWiki $twikiBranch built OK\n\n see http://fosiki.com/$twikiBranch/ for output files.\n".$buildOutput);
+	`scp TWiki* fosiki\@fosiki.com:/home/distributedinformation/www/$twikiBranch` ;
 }
 
 
