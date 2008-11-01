7988c435328c0ac739819e478017b0fb0007b281
SvenDowideit <SvenDowideit>
2008-11-01 13:45:38 +0000

    import FirebugLitePlugin

---
+++ FirebugLitePlugin/lib/TWiki/Plugins/FirebugLitePlugin.pm
@@ -0,0 +1,105 @@
+# Plugin for TWiki Collaboration Platform, http://TWiki.org/
+#
+# (c) 2008 SvenDowideit@home.org.au
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+
+=pod
+
+---+ package TWiki::Plugins::FirebugLitePlugin
+
+
+=cut
+
+
+package TWiki::Plugins::FirebugLitePlugin;
+
+# Always use strict to enforce variable scoping
+use strict;
+
+require TWiki::Func;    # The plugins API
+require TWiki::Plugins; # For the API version
+
+# $VERSION is referred to by TWiki, and is the only global variable that
+# *must* exist in this package.
+use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+
+# This should always be $Rev$ so that TWiki can determine the checked-in
+# status of the plugin. It is used by the build automation tools, so
+# you should leave it alone.
+$VERSION = '$Rev$';
+
+# This is a free-form string you can use to "name" your own plugin version.
+# It is *not* used by the build automation tools, but is reported as part
+# of the version number in PLUGINDESCRIPTIONS.
+$RELEASE = 'TWiki-4.2';
+
+# Short description of this plugin
+# One line description, is shown in the %TWIKIWEB%.TextFormattingRules topic:
+$SHORTDESCRIPTION = 'FirebugLite javascript based debugger for IE, Opera, Safari and FireFox';
+
+# You must set $NO_PREFS_IN_TOPIC to 0 if you want your plugin to use preferences
+# stored in the plugin topic. This default is required for compatibility with
+# older plugins, but imposes a significant performance penalty, and
+# is not recommended. Instead, use $TWiki::cfg entries set in LocalSite.cfg, or
+# if you want the users to be able to change settings, then use standard TWiki
+# preferences that can be defined in your Main.TWikiPreferences and overridden
+# at the web and topic level.
+$NO_PREFS_IN_TOPIC = 1;
+
+# Name of this Plugin, only used in this module
+$pluginName = 'FirebugLitePlugin';
+
+=pod
+
+---++ initPlugin($topic, $web, $user, $installWeb) -> $boolean
+   * =$topic= - the name of the topic in the current CGI query
+   * =$web= - the name of the web in the current CGI query
+   * =$user= - the login name of the user
+   * =$installWeb= - the name of the web the plugin is installed in
+
+
+
+
+=cut
+
+sub initPlugin {
+    my( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $TWiki::Plugins::VERSION < 1.026 ) {
+        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+        return 0;
+    }
+
+    # Example code of how to get a preference value, register a variable handler
+    # and register a RESTHandler. (remove code you do not need)
+
+    # Set plugin preferences in LocalSite.cfg, like this:
+    # $TWiki::cfg{Plugins}{FirebugLitePlugin}{ExampleSetting} = 1;
+    # Always provide a default in case the setting is not defined in
+    # LocalSite.cfg. See TWiki.TWikiPlugins for help in adding your plugin
+    # configuration to the =configure= interface.
+    my $setting = $TWiki::cfg{Plugins}{FirebugLitePlugin}{ExampleSetting} || 0;
+    $debug = $TWiki::cfg{Plugins}{FirebugLitePlugin}{Debug} || 0;
+
+    TWiki::Func::loadTemplate(lc($pluginName));
+    my $firebugHeader = TWiki::Func::expandTemplate($debug?'firebug':'firebug-compressed');
+    TWiki::Func::addToHEAD($pluginName, $firebugHeader);
+
+    # Plugin correctly initialized
+    return 1;
+}
+
+
+
+1;
