627199e55f7510f5ba12feefea58dd88b96b87f1
SvenDowideit <SvenDowideit>
2008-11-01 22:36:11 +0000

    import TdbContrib

---
+++ TdbContrib/lib/tdb/acconfig.h
@@ -0,0 +1,9 @@
+#undef NDEBUG /* assert.h isn't used atm. */
+#undef STANDALONE
+#undef USE_SPINLOCKS
+#undef SPARC_SPINLOCKS
+#undef INTEL_SPINLOCKS
+#undef MIPS_SPINLOCKS
+#undef POWERPC_SPINLOCKS
+#undef PACKAGE
+#undef VERSION
