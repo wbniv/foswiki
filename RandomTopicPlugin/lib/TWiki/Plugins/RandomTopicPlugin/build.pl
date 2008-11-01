6d76ddab2fa3098e55cab2addae61cf283b25af8
SvenDowideit <SvenDowideit>
2008-11-01 19:38:08 +0000

    import RandomTopicPlugin

---
+++ RandomTopicPlugin/lib/TWiki/Plugins/RandomTopicPlugin/build.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+#
+# Read the comments at the top of lib/TWiki/Plugins/Build.pm for
+# details of how the build process works, and what files you
+# have to provide and where.
+#
+BEGIN {
+    foreach my $pc (split(/:/, $ENV{TWIKI_LIBS})) {
+        unshift @INC, $pc;
+    }
+}
+
+use TWiki::Contrib::Build;
+
+# Create the build object
+$build = new TWiki::Contrib::Build( "RandomTopicPlugin" );
+
+# Build the target on the command line, or the default target
+$build->build($build->{target});
+
