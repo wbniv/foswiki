a0c7147a4b82142965bf977441f5071bb66a71d3
SvenDowideit <SvenDowideit>
2008-11-01 20:36:44 +0000

    import SignaturePlugin

---
+++ SignaturePlugin/lib/TWiki/Plugins/SignaturePlugin.pm
@@ -0,0 +1,76 @@
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2006 Motorola, thomas.weigert@motorola.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For licensing info read LICENSE file in the TWiki root.
+
+package TWiki::Plugins::SignaturePlugin;
+
+# Always use strict to enforce variable scoping
+use strict;
+
+# $VERSION is referred to by TWiki, and is the only global variable that
+# *must* exist in this package
+use vars qw( $VERSION $RELEASE $debug $pluginName );
+
+# This should always be $Rev: 0$ so that TWiki can determine the checked-in
+# status of the plugin. It is used by the build automation tools, so
+# you should leave it alone.
+$VERSION = '$Rev: 0$';
+
+# This is a free-form string you can use to "name" your own plugin version.
+# It is *not* used by the build automation tools, but is reported as part
+# of the version number in PLUGINDESCRIPTIONS.
+$RELEASE = 'Dakar';
+
+# Name of this Plugin, only used in this module
+$pluginName = 'SignaturePlugin';
+
+sub initPlugin {
+    my( $topic, $web, $user, $installWeb ) = @_;
+
+    if( $TWiki::Plugins::VERSION < 1.1 ) {
+        TWiki::Func::writeWarning( "This version of $pluginName works only with TWiki 4 and greater." );
+        return 0;
+    }
+
+    # Get plugin debug flag
+    $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
+
+    # Plugin correctly initialized
+    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+    return 1;
+
+}
+
+sub preRenderingHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0], $_[1] instead
+
+    &TWiki::Func::writeDebug( "- $pluginName::preRenderingHandler" ) if $debug;
+
+    # This handler is called by getRenderedVersion just before the line loop
+    # Only bother with this plugin if viewing (i.e. not searching, etc)
+    return unless ($0 =~ m/view|viewauth|render/o);
+
+    my $cnt;
+    $_[0] =~ s/%SIGNATURE(?:{(.*)})?%/&handleSignature($cnt++, $1)/geo;
+
+}
+
+sub handleSignature {
+  require TWiki::Plugins::SignaturePlugin::Signature;
+  return TWiki::Plugins::SignaturePlugin::Signature::handleSignature(@_);
+}
+
+1;
