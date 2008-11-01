757260a599c062e8076a396174372e68b8a43d70
SvenDowideit <SvenDowideit>
2008-11-01 18:54:21 +0000

    import PingBackPlugin

---
+++ PingBackPlugin/lib/TWiki/Plugins/PingBackPlugin/build.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+BEGIN {
+  foreach my $pc (split(/:/, $ENV{TWIKI_LIBS})) {
+    unshift @INC, $pc;
+  }
+}
+
+use TWiki::Contrib::Build;
+
+# Create the build object
+my $build = new TWiki::Contrib::Build( 'PingBackPlugin' );
+
+# Build the target on the command line, or the default target
+my $build->build($build->{target});
+
