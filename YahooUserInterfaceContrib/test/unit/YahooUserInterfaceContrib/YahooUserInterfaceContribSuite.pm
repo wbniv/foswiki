17001170c6c2e23c98443deea084690bad5db150
SvenDowideit <SvenDowideit>
2008-11-02 01:04:35 +0000

    import YahooUserInterfaceContrib

---
+++ YahooUserInterfaceContrib/test/unit/YahooUserInterfaceContrib/YahooUserInterfaceContribSuite.pm
@@ -0,0 +1,9 @@
+package YahooUserInterfaceContribSuite;
+
+use base qw(Unit::TestSuite);
+
+sub name { 'YahooUserInterfaceContribSuite' };
+
+sub include_tests { qw(YahooUserInterfaceContribTests) };
+
+1;
