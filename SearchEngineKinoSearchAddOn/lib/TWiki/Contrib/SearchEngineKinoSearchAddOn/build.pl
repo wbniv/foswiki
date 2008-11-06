486056369fc560cdc26ebd46c51577a8da34ccc6
SvenDowideit <SvenDowideit>
2008-11-06 12:17:49 +0000

    Item97: update to latest build.pl

--- SearchEngineKinoSearchAddOn/lib/TWiki/Contrib/SearchEngineKinoSearchAddOn/build.pl
+++ SearchEngineKinoSearchAddOn/lib/TWiki/Contrib/SearchEngineKinoSearchAddOn/build.pl
@@ -1,26 +1,52 @@
 #!/usr/bin/perl -w
+#
+# Example build class. Copy this file to the equivalent place in your
+# plugin or contrib and edit.
+#
+# Read the comments at the top of lib/TWiki/Contrib/Build.pm for
+# details of how the build process works, and what files you
+# have to provide and where.
+#
+# Requires the environment variable TWIKI_LIBS (a colon-separated path
+# list) to be set to point at the build system and any required dependencies.
+# Usage: ./build.pl [-n] [-v] [target]
+# where [target] is the optional build target (build, test,
+# install, release, uninstall), test is the default.`
+# Two command-line options are supported:
+# -n Don't actually do anything, just print commands
+# -v Be verbose
+#
+
+# Standard preamble
 BEGIN {
     unshift @INC, split( /:/, $ENV{TWIKI_LIBS} );
 }
+
 use TWiki::Contrib::Build;
 
+# Declare our build package
+package BuildBuild;
+use base qw( TWiki::Contrib::Build );
+
+sub new {
+    my $class = shift;
+    return bless( $class->SUPER::new( "BuildContrib", "Build" ), $class );
+}
+
+# Example: Override the build target
+sub target_build {
+    my $this = shift;
+
+    $this->SUPER::target_build();
+
+    # Do other build stuff here
+}
+
+package main;
+
 # Create the build object
-$build = new TWiki::Contrib::Build('SearchEngineKinoSearchAddOn');
-
-# (Optional) Set the details of the repository for uploads.
-# This can be any web on any accessible TWiki installation.
-# These defaults will be used when expanding tokens in .txt
-# files, but be warned, they can be overridden at upload time!
-
-# name of web to upload to
-$build->{UPLOADTARGETWEB} = 'Plugins';
-# Full URL of pub directory
-$build->{UPLOADTARGETPUB} = 'http://twiki.org/p/pub';
-# Full URL of bin directory
-$build->{UPLOADTARGETSCRIPT} = 'http://twiki.org/cgi-bin';
-# Script extension
-$build->{UPLOADTARGETSUFFIX} = '';
+$build = new BuildBuild();
 
 # Build the target on the command line, or the default target
-$build->build($build->{target});
+$build->build( $build->{target} );
 
