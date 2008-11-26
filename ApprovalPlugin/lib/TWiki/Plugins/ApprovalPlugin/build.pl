d3b86815db83b3c4d866f6b2af73688ab7f00924
AndrewJones <AndrewJones>
2008-11-26 15:05:00 +0000

    Item6113: doc changes for foswiki

--- ApprovalPlugin/lib/TWiki/Plugins/ApprovalPlugin/build.pl
+++ ApprovalPlugin/lib/TWiki/Plugins/ApprovalPlugin/build.pl
@@ -2,10 +2,10 @@
 BEGIN {
     unshift @INC, split( /:/, $ENV{TWIKI_LIBS} );
 }
-use TWiki::Contrib::Build;
+use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build('ApprovalPlugin');
+$build = new Foswiki::Contrib::Build('ApprovalPlugin');
 
 # (Optional) Set the details of the repository for uploads.
 # This can be any web on any accessible TWiki installation.
