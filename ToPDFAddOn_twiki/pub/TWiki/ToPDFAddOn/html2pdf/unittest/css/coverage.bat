d5db189f1d542e84560c1f6b2dd01dc815b32dae
EugenMayer <EugenMayer>
2008-12-01 17:34:27 +0000

    Item374: deleting old ToPDFAddon

--- ToPDFAddOn_twiki/pub/TWiki/ToPDFAddOn/html2pdf/unittest/css/coverage.bat
+++
@@ -1,3 +0,0 @@
-@set PATH=%PATH%;c:\php\php5.2.1\
-@set PHPUNIT=c:\php\php5.2.1\phpunit.bat
-%PHPUNIT% --report report HTML2PS_CSSTests suite.php
--- ToPDFAddOn_twiki/pub/TWiki/ToPDFAddOn/html2pdf/unittest/css/run.bat
+++
@@ -1,2 +0,0 @@
-@set PATH=%PATH%;c:\php\php5.2.1\
-php suite.php 2>&1 | tee suite.log
